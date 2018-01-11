241b98615f580781f94dfc7382efb85e35a72e9d
imanurft <imanurfitriani@gmail.com>
2018-01-12 02:01:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fanzy/formlogin/Login1.java
@@ -0,0 +1,13 @@
+package com.example.fanzy.formlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login1);
+    }
+}
